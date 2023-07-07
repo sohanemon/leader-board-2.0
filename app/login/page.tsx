822a160afafd1713f6758fe87d520d94a7120c70
sohanemon <m.sohanemon@gmail.com>
2023-07-07 14:54:10 +0600

    fix: deploy err

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,7 +9,6 @@ import { useRef } from 'react';
 
 export default function Page() {
   const searchParams = useSearchParams();
-  const { firebaseErr } = useStore();
   const formRef = useRef<HTMLFormElement>(null);
   const signUp = searchParams.get('signup');
 
@@ -30,7 +29,6 @@ export default function Page() {
         <Input name='email' size='lg' type='email' label='Email' />
         <Input name='password' size='lg' type='password' label='Password' />
 
-        {firebaseErr && <p className='text-red-500 my-2'> * {firebaseErr}</p>}
         <Btn onClick={handleSubmit}> {signUp ? 'Sign Up' : 'Sign In'}</Btn>
         {!signUp ? (
           <p className='text-sm text-center'>
